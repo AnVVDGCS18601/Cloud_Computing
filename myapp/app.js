af4b786bb86a4f3ef9652e29217de2d183a25f0b
Võ Vương Đại An <anvvd@metanomy.asia>
2023-06-19 14:25:08 +0700

    express view generator

--- myapp/app.js
+++ myapp/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
+var aboutsRouter = require('./routes/abouts');
 
 var app = express();
 
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/home', homeRouter);
+app.use('/about', aboutsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- myapp/routes/home.js
+++ myapp/routes/home.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.writeHead(200);
+  res.end("<h1>This is home page</h1>");
 });
 
 module.exports = router;
